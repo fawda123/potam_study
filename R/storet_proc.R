d40adc6525e199ab4da193fbb103071334cb3e43
fawda123 <mbafs2012@gmail.com>
2015-01-17 15:32:19 -0600

    More processing of storet, station is not wbic for WI data

--- R/storet_proc.R
+++ R/storet_proc.R
@@ -1,7 +1,12 @@
+######
+# processing of STORET wq data for MN and WI
+# January 2015
 
 ######
 # get legacy storet data (up to 1998) from downloaded files for MN
 # downloaded from here: http://www.epa.gov/storpubl/legacy/gateway.htm
+
+rm(list = ls())
 
 library(foreach)
 library(doParallel)
@@ -127,4 +132,131 @@ allmn_wq <- rbind(dat, tmp)
 
 # aggregated 
 allmn_wq <- aggregate(. ~ lake, allmn_wq[, -2], FUN = function(x) mean(x, na.rm = T), na.action = na.pass)
+
+save(allmn_wq, file = 'data/allmn_wq.RData')
+
+######
+# WI storet data
+# get legacy storet data (up to 1998) from downloaded files for WI
+# downloaded from here: http://www.epa.gov/storpubl/legacy/gateway.htm
+
+rm(list = ls())
+
+library(foreach)
+library(doParallel)
+
+load('data/wi_potam.RData')
+
+get_files <- list.files(path = 'M:/docs/veg_indics/STORET_data/WI_legacy_storet/Wisconsin', 
+  pattern = '^.*_res_.*\\.txt', recursive = T, full.names = T)
+
+strt <- Sys.time()
+wi_lakes <- wi_potam$lake
+
+cl <- makeCluster(8)
+registerDoParallel(cl)
+
+out_dat <- foreach(fl = get_files) %dopar% {
+  
+  # counter
+  sink('C:/Users/mbeck/Desktop/log.txt')
+  cat(basename(fl), '\n')
+  print(Sys.time() - strt)
+  sink()
+  
+  # get file
+  tmp <- readLines(fl)
+  heads <- strsplit(tmp[1], '\t')[[1]]
+  heads <- gsub('^[ ]*|[ ]*$', '', heads)
+
+  # parse, clear trailing/leading spaces
+  tmp <- strsplit(tmp, '\\t')
+  tmp <- suppressWarnings(do.call('rbind', tmp))
+  tmp <- gsub('^[ ]*|[ ]*$', '', tmp)
+  tmp <- data.frame(tmp[-c(1:2), ], stringsAsFactors = F)[, 1:19]
+  names(tmp) <- heads[1:19]
+  
+  # filter by phosphorus, color, secchi, and alkalinity
+  # secchi: 77-78
+  # color: 79-84
+  # alkalinity: 409-431
+  # phosphorus: 650-678
+  get_parms <- as.numeric(as.character(tmp$Param)) %in% c(77:84, 409:431, 650:678)
+  tmp <- tmp[get_parms, ]
+  
+  # format stations
+  tmp$Station <- gsub('^0*|-', '', as.character(tmp$Station))
+  tmp <- tmp[!grepl('[A-Z,a-z]', tmp$Station), ]
+  tmp$Station <- as.numeric(tmp$Station)
+  
+  tmp$result <- as.numeric(as.character(tmp[, 'Result Value']))
+  
+  # retain columns of interest
+  keep_cols <- c('Station', 'Param', 'Start Date', 'result')
+  tmp <- tmp[, keep_cols]
+  names(tmp) <- c('lake', 'param', 'date', 'value')
+  
+  # subset by veg dows
+#   tmp <- tmp[tmp$lake %in% wi_lakes, ]
+  
+  tmp
+    
+}
+
+
+# combine, average by day for each lake, make wide format
+tmp <- do.call('rbind', out_dat)
+tmp <- tmp[as.numeric(tmp$param) %in% c(78, 80, 410, 665), ]
+tmp <- dcast(tmp, lake + date ~ param, value.var = 'value', 
+  fun.aggregate = function(x) mean(x, na.rm = T))
+names(tmp) <- c('lake', 'date', 'secchi', 'color', 'alk', 'tp')
+
+
+######
+# current WI storet
+# import raw data
+raw_dat <- 'M:/docs/veg_indics/STORET_data/WI_current_storet.txt'
+raw_dat <- read.table(raw_dat, sep = '\t', header = T)
+
+# station and wbic matches from GIS intersect, only for recent STORET data
+# named 'keys'
+load(file = 'M:/docs/veg_indics/STORET_data/wi_recentstoret_keys.RData')
+
+# select columns of interest from STORET
+keep_cols <- c('Station.ID', 'Activity.Start', 'Characteristic.Name', 'Result.Value.as.Text', 'Units')
+dat <- raw_dat[, keep_cols]
+names(dat) <- c('station', 'date', 'param', 'value', 'units')
+    
+# merge station data with keys
+dat <- dplyr::inner_join(dat, keys, by = 'station')
+
+# convert param column
+dat$param <- as.character(dat$param)
+dat$param[grepl('Alkalinity', dat$param)] <- 'alk'
+dat$param[grepl('Phosphorus', dat$param)] <- 'TP'
+dat$param[grepl('Color|color', dat$param)] <- 'color'
+dat$param[grepl('Transparency|Secchi', dat$param)] <- 'secchi'
+dat <- dat[dat$param %in% c('alk', 'TP', 'color', 'secchi'), ]
+
+###### 123453145234asdas5
+# need to figure out unit conversions on this for aggregation
+
+# repeat the exercise for legacy data (i.e., find stations that are within a wbic)
+
+
+
+
+
+
+
+
+# aggregate by day
+dat$date <- as.POSIXct(as.character(dat$date), format = '%Y-%m-%d %H:%M:%S')
+dat$date <- as.Date(dat$date)
+dat <- dcast(dat, wbic + date ~ param, value.var = 'value', 
+  fun.aggregate = function(x) mean(as.numeric(as.character(x)), na.rm = T))
+
+# get only the lakes that I have
+load('data/wi_potam.RData')
+dat <- dat[dat$wbic %in% wi_potam$lake, ]
 
