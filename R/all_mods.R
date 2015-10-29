75d19c31ba21a7127d6f28edf82090b9bda81717
fawda123 <mbafs2012@gmail.com>
2015-10-29 16:11:42 -0500

    model skeletons, not correct but idea is ok

--- R/all_mods.R
+++ R/all_mods.R
@@ -1,9 +1,18 @@
+######
+# reproducing pRDA, pLR, and GLM for potamogeton analysis
+# this is not exactly Janne's analysis but similar for my knowledge
+# it's generally complete except for tabulation of variance partitioning
+# he used an additional step using adjusted R-squared
+
 library(dplyr)
 library(vegan)
 library(packfor)
-
+library(MASS)
 
 load(file = 'data/all_potam.RData')
+
+######
+# community composition model
 
 # get species distribution/abund matrix
 # use hellinger transformation (divide by margin totals then sqrt)
@@ -25,3 +34,58 @@ spa <- spa[, names(spa) %in% spa_sel$variables]
 
 cc_mod <- varpart(Y = pot, X = loc, cli, spa)
   
+######
+# richness model
+
+# Janne used sum of all abundances as richness
+rich <- select(all_potam, matches('^P', ignore.case = F)) %>% 
+  rowSums %>% 
+  data.frame(all_potam, S = .)
+
+##
+# separate mods  
+
+# local mod
+rich_modloc <- glm(S ~ color + depth + tp + alk + secchi, family = poisson(link = 'log'), 
+  data = rich, offset = log(tot))
+
+# climate mod
+rich_modcli <- glm(S ~ prec + tmax + tmean + alt + tmin, family = poisson(link = 'log'), 
+  data = rich, offset = log(tot))
+
+# spatial mod
+form <- grep('^V', names(rich), value = T) %>% 
+  paste(., collapse = ' + ') %>% 
+  paste0('S ~ ', .) %>% 
+  as.formula
+rich_modspa <- glm(form, data = rich, family = poisson(link = 'log'), offset = log(tot))
+
+##
+# partial least squares, using varpart 
+rich_mod <- varpart(Y = rich$S, X = loc, cli, spa)
+
+######
+# individual species mods, repeated for each species
+
+# local mod
+PA_modloc <- glm(PA ~ color + depth + tp + alk + secchi, family = poisson(link = 'log'), 
+  data = rich, offset = log(tot)) %>% 
+  stepAIC(PA_modloc, direction = 'both', criterion = 'AIC')
+PA_modloc <- formula(PA_modloc)[c(1, 3)]
+
+# climate mod
+PA_modcli <- glm(PA ~ prec + tmax + tmean + alt + tmin, family = poisson(link = 'log'), 
+  data = rich, offset = log(tot)) %>% 
+  stepAIC(., direction = 'both', criterion = 'AIC')
+PA_modcli <- formula(PA_modcli)[c(1, 3)]
+
+# spatial mod, stepwise, then get formula
+form <- grep('^V', names(rich), value = T) %>% 
+  paste(., collapse = ' + ') %>% 
+  paste0('PA ~ ', .) %>% 
+  as.formula
+PA_modspa <- glm(form, data = rich, family = poisson(link = 'log'), offset = log(tot)) %>% 
+  stepAIC(., direction = 'both', criterion = 'AIC')
+PA_modspa <- formula(PA_modspa)[c(1, 3)]
+
+varpart(rich$PA, PA_modloc, PA_modcli, PA_modspa, transfo = 'hel', data = rich)
